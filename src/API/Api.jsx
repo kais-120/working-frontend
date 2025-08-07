082e0fc05d3d99213adf5e2f145c16d9de6e4b9e
kais-120 <kaisbennour7@gmail.com>
2025-08-07 16:30:05 +0100

    change server

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -3,9 +3,9 @@ import Cookies from "universal-cookie";
 
 const cookie = new Cookies();
 const token = cookie.get("auth")
-export const API_URL = "http://localhost:5000/api/v1";
-export const SOCKET_URL = "http://localhost:5000"
-export const IMAGE_URL = "http://localhost:5000/uploads"
+export const API_URL = "https://working-ohu1.onrender.com/api/v1";
+export const SOCKET_URL = "https://working-ohu1.onrender.com"
+export const IMAGE_URL = "https://working-ohu1.onrender.com/uploads"
 export const  Axios = axios.create({baseURL:API_URL,
     headers:{
         "x_api_key":import.meta.env.VITE_API_KEY
